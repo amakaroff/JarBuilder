9041e1eda18545df9a0a12b3ab1a4e264f4c8001
amakaroff <alekseymakarov1996@gmail.com>
2017-06-14 00:24:05 +0400

    fixed create jar file

--- src/com/makarov/builder/AgentJarBuilder.java
+++ src/com/makarov/builder/AgentJarBuilder.java
@@ -94,7 +94,7 @@ public class AgentJarBuilder {
             }
 
             File file = new File(jarPath);
-            while (!file.isFile());
+            while (!file.exists());
             return jarPath;
         }
 
